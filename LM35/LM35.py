add66cae521fcd87cdc282bd36a57b32bd5cab6f
Hennakk <98154707+Hennakk@users.noreply.github.com>
2022-02-09 02:41:27 +0900

    Update LM35.py

--- LM35/LM35.py
+++ LM35/LM35.py
@@ -10,12 +10,18 @@ bus = smbus.SMBus(1)
 while True :
     bus.write_byte(address, A0)
     value = bus.read_byte(address)
-*   value = (value*1.92) 
+    value = (value*1.92) 
     temp = round(value/10,2)
     print(temp)
     time.sleep(0.2)
-/*
+'''
 PCF8591모듈은 8bit의 아날로그 할당비트를 가져 ADC정밀도가 낮다.
 500mV(50도) 이하의 낮은 전압체서는 최대 100mV의 오차가 발생한다. 
 이러한 오차를 보정하기 위해 온도계산식을 이용하여 계산한 후 10으로 나누어 주어 10배의 오차율을 줄여준다.
-*/
+'''
+
+'''
+*  ADC data :
+*  1.961 : ADC 8bit(256 / 0~5V)
+*  1.294 : ADC 8bit(256 / 0~3.3V)
+'''
